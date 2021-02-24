419b65a04650e7e9bfa534cef58b32288d45c692
Kermitofx <39018448+Kermitofx@users.noreply.github.com>
2021-02-24 10:31:24 -0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,4 @@
-bot_token = "1488727769:AAElRU7yS309GP1t54F635ASDF"
+bot_token = "1512685532:AAEYmJNYUG66Gq04ii2imfOYUk1JvqnBsyM"
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
-sudo_chat_id = -1001485876964
-owner_id = 1243703097
+sudo_chat_id = -1001172749065
+owner_id = 957539786
